df32ad3bc873b6282a50271e28ba5420a1501380
Divyarani <patildivyarani12@gmail.com>
2026-03-11 17:32:03 +0530

    .day8

--- Day7/Exercise1/obj/Debug/net10.0/Exercise1.AssemblyInfo.cs
+++ Day7/Exercise1/obj/Debug/net10.0/Exercise1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1fb27159827c72317dd0e9ae85bb16e7c3f1e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55669e6d9c6aca7f6dac27f7020f928a61272821")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
